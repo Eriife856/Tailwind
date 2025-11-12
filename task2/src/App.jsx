62b06d5d4b7afc6c9e56b29f5d0877c75915ec37
fopefoluwa <ojodorcas57@gmail.com>
2025-11-12 10:48:52 +0100

    task 2 added

---
+++ task2/src/App.jsx
@@ -0,0 +1,17 @@
+import ButtonComponentLibrary from './ButtonComponentLibrary';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <div className="min-h-screen bg-gray-100 p-8 space-y-10"></div>
+        <ButtonComponentLibrary/>
+      </div>
+      
+  );
+}
+
+export default App;
